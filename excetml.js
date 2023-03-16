bb2c3885926b70ffa1aef425f46d33d65ca371c9
Henrisen <56893232+Henrisen@users.noreply.github.com>
2023-03-16 10:22:26 +0100

    Update excetml.js

--- excetml.js
+++ excetml.js
@@ -1,8 +1,12 @@
+let params = new URL(document.location).searchParams;
+let size = params.get("size");
+size = parseInt(size)
+
 const cel = e => document.createElement(e)
 
 t = cel("table")
 let trh = cel("tr")
-for (let i=0; i<9999; i++) {
+for (let i=0; i<size; i++) {
     let td = cel("td")
     td.innerText += i+1
     trh.appendChild(td)
@@ -10,12 +14,12 @@ for (let i=0; i<9999; i++) {
 
 t.appendChild(trh)
 
-for (let i = 0; i < 9999; i++) {
+for (let i = 0; i < size; i++) {
     let tr = cel("tr")
     th = cel("th")
     th.innerText += i+1
     tr.appendChild(th)
-    for (let j = 0; j < 9999; j++) {
+    for (let j = 0; j < size; j++) {
         let td = cel("td")
         let ip = cel("input")
         ip.id = "c" + j + "r" + i
